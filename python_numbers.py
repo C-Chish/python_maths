0492043a8058cf62dc0c2c988097e1acd73f3d6b
Carolina Falado <togykip@gmail.com>
2019-06-13 15:31:37 +1200

    Python tutorial 4 update

--- python_numbers.py
+++ python_numbers.py
@@ -1,32 +1,238 @@
-# Basic-Arithmetic-Operators
-# Shortcut for remembering quick operators
+# Numbers, Lists, Tuples, Sets
 
-# Arithmetic Operators:
-# Addition:       3 + 2
-# Substraction:   3 - 2
-# Multiplication: 3 * 2
-# Division:       3 / 2     -Does not drop decimal
-# Floor Division: 3 // 2    -Drops decimal
-# Exponent:       3 ** 2    -To the power of
-# Modulus         3 % 2     -Gives remainder after a division
-# No remainders=  Number is even
-# Remainders = Number is uneven
+# numbers that look like strings
+# vars that look like numbers
 
-# Order of operation, used with parenthesis. eg:
-# print(3 * 2 + 1)
-# print(3 * (2 + 1))
+# num_1 = '100'
+# num_2 = '200'  # this is a string lol
 
-# num = 2
-# num *= 10
-# print(num)
+# print(num_1 + num_2)
+# these two have bee inated
 
-# ABS (abs)- absolute value "the magnitude of a real number without
-# regard to its sign."
-# print(abs(-3))
+# ~CASTING
+# to turn them into intergers
 
-# Round- rounding numbers to the nearest interger value
-# print(round(1.23)) = 1
-# Rounding digits to the first decimal
-# print(round(1.266, 1)) = 1.3
+# num_1 = int(num_1)
+# num_2 = int(num_2)  # this is a functiont o create an interger
 
-# Comparisons Operators
+# print(num_1 + num_2)
+
+# Python Tutorial for Beginners 4: Lists, Tuples, and Sets
+
+# ~LISTS[]
+# work with a list of values
+
+# courses = ['Python', 'C#', 'Ruby', 'GIT']
+# print(courses[2])
+
+# Negative index- -1 will always be the last item.
+# Index error- no item
+# print(courses[-1])
+
+# ~SLICING
+
+# we want all the values starting from the beginning
+# and up to but not including eg:
+# print(courses[0:2])  # should print out python and C#
+# you can leave out the 0- it assumes you start from the
+# beginning
+
+# if nothing is put next to the : , it assumes it wants to go to
+# the end of the list
+
+# ~APPEND
+# Adds to the list, puts it at the end
+# courses.append('Atom')
+# print(courses)
+# ['Python', 'C#', 'Ruby', 'GIT', 'Atom']
+
+# ~INSERT
+# Insert adds it to a specidic location
+# Takes two arguements (the index you want to insert it)
+# (location : value) eg:
+# print(courses)
+# courses.insert(1, 'Atom')
+
+# print(courses)
+# ['Python', 'C#', 'Ruby', 'GIT']
+# ['Python', 'Atom', 'C#', 'Ruby', 'GIT']
+
+# ~EXTEND
+# for multiple values we want to add to our list
+# courses_2 = ['cooking', 'karate']
+# courses.insert(0, courses_2)
+# print(courses)
+# [['cooking', 'karate'], 'Python', 'C#', 'Ruby', 'GIT']
+
+# INSERT in this case inserts the whole list
+# instead of individual values inside the list
+# So instead use INSERT
+# courses.extend(courses_2)
+# print(courses)
+# ['Python', 'C#', 'Ruby', 'GIT', 'cooking', 'karate']
+
+# ~REMOVE
+# removing values from our list
+# courses.remove('C#')
+# print(courses)
+
+# courses = ['Python', 'C#', 'Ruby', 'GIT']
+
+# or you can use ~POP
+# courses.pop()
+# removes the last item on the lsit
+# print(courses)
+# ['Python', 'C#', 'Ruby'](removed GIT)
+# pop also prints out the value it removed
+# deleted = courses.pop()
+# print(deleted)  # GIT
+# print(courses)  # ['Python', 'C#', 'Ruby']
+# courses = ['Python', 'C#', 'Ruby', 'GIT']
+
+# ~REVERSE
+# courses.reverse()
+
+# ~SORTING
+# Sorts alphabetically
+# courses.sort()
+# print(courses)
+# ['C#', 'GIT', 'Python', 'Ruby']
+
+# Also sorts numbers in ascending order
+# nums = [4, 5, 8, 4, 22, 0]  # [0, 4, 4, 5, 8, 22]
+# nums.sort()
+# print(nums)
+
+# Decending Order
+# courses.sort(reverse=True)
+# nums.sort(reverse=True)
+# print(nums)
+# print(courses)
+# [22, 8, 5, 4, 4, 0]
+# ['Ruby', 'Python', 'GIT', 'C#']
+# These alters the originals
+
+# SORTED function (different to the sort method)
+# sorted(nums)
+# print(nums)
+# list is not sorted it's the same
+# this is beacause the ~sorted function does not sort list in place
+# returns a sorted version of the list
+# [4, 5, 8, 4, 22, 0]
+# sorted_nums = sorted(nums)
+# print(sorted_nums)
+# [0, 4, 4, 5, 8, 22] - sorted
+# this does not alter the originals
+
+# ~MIN AND MAX/SUM
+# print(min(nums))  # 0
+# print(max(nums))  # 22
+# print(sum(nums))  # 43
+
+# Finding the ~INDEX VALUE in our list
+# courses = ['Python', 'C#', 'Ruby', 'GIT']
+# print(courses.index('GIT'))  # index 2
+
+# To check if a value is even in our list and to check
+# if a value is true or false: "IN FUNCTION"
+# print('Art' in courses)  # returns false because 'Art' is not on the list
+
+# ~FOR LOOP
+# for item in courses:
+# print(item)   # we want to print out that item
+# Creating a loop that goes through each value of the list
+# Each loop through this item variable will be
+# equal to the next item in the list
+# The indentation is to say the code is excecuted from within the for loop
+# "item" is just a var, it can be changed
+
+# ~ENUMERATE function
+# for index, item in enumerate(courses):
+#    print(index, item)  # returns:
+# 0 Python
+# 1 C#
+# 2 Ruby
+# 3 GIT
+# if we do not want to start from 0
+# we can pass in a start value to "enumerate" function
+
+# for index, item in enumerate(courses, start=1):
+# print(index, item)  # returns starting value as 1
+
+# ~JOIN
+# Turning lists into str's separated by a values
+courses = ['Python', 'C#', 'Ruby', 'GIT']
+# courses_str = ' - '.join(courses)
+# print(courses_str)
+# Returns {Python -C# -Ruby -GIT}
+
+# ~SPLIT
+# new_list = courses_str.split(' - ')  # split up the values on this space,
+# print(new_list)
+# Returns ['Python', 'C#', 'Ruby', 'GIT']
+
+# TUPLES() & SETS{}
+# similar to lists but can't modify them
+# mutable eg: "lists" inmutable eg: "turps"
+
+# inmutable- not many methods to use, can loop, access values.
+# can't be modified
+# tuple1 = ('Hugs', 'Food', 'Stuff')
+# tuple2 = tuple1
+# print(type(tuple1))
+# print(tuple1)
+# print(tuple2)
+# if we were to try modify it:
+# tuple1[1] = 'Cake' -it woudl return
+# TypeError: 'tuple' object does not support item assignment
+# pro tip- ctrl + / = comment out code fast
+
+# ~SETS
+# do not care about order and always change {} eg:
+# set_list = {'home', 'food', 'sleep}
+# print(set_list)
+# results in:{'food', 'home', 'sleep'}
+# main uses for a tets is to testif a value is part of a set
+# and to remove duplicate values eg:
+# set_list = {'home', 'food', 'food', 'sleep'}
+# print(set_list)
+# returns with {'food', 'home', 'sleep'}
+# Tests if a value is part of a set "membership test"
+# Membership tests can be done with lists + tuples too
+# Sets are optimized for this
+# print(set_list)
+# within print statement- eg
+# print('home' in set_list)   # returns True
+
+# ~INTERSECTION
+# checks similarities in both sets eg:
+# set_list = {'home', 'food', 'food', 'sleep'}
+# inter_list = {'dogs', 'food', 'cats', 'sleep'}
+# print(set_list.intersection(inter_list))
+# returns: {'food', 'sleep'} as these are in the same set
+
+# ~DIFFERENCE
+# checks differences in sets eg:
+# set_list = {'home', 'food', 'food', 'sleep'}
+# inter_list = {'dogs', 'food', 'cats', 'sleep'}
+# print(set_list.difference(inter_list))
+# # returns: {'home'}
+
+# ~UNION method
+# print out all sets, conjointed together eg:
+# set_list = {'home', 'food', 'food', 'sleep'}
+# inter_list = {'dogs', 'food', 'cats', 'sleep'}
+# print(set_list.union(inter_list))
+# returns {'food', 'sleep', 'dogs', 'cats', 'home'}
+
+# # Empty Lists
+# empty_list = []
+# empty_list = list()
+
+# # Empty Tuples
+# empty_tuple = ()
+# empty_tuple = tuple()
+
+# # Empty Sets
+# empty_set = {}  # This isn't right! It's not an empty set its a dictionary
+# empty_set = set()  # Right way to do it
